5fecaca1c7da9e748bf12eb1b155a13924b4fe0f
Liya Sharipova <liyasharipova2107@gmail.com>
2015-03-15 21:06:10 +0400

    исправляю глупости

--- LinkedList/src/ru/kpfu/itis/group11403/sharipova/linkedlist/MyLinkedList.java
+++ LinkedList/src/ru/kpfu/itis/group11403/sharipova/linkedlist/MyLinkedList.java
@@ -1,11 +1,12 @@
 package ru.kpfu.itis.group11403.sharipova.linkedlist;
 
+
+
 public class MyLinkedList<T> {
-	private int size=0;
 
 	private class Item {
 		private T obj;
-		
+
 		private Item next;
 
 		public Item getNext() {
@@ -29,27 +30,32 @@ public class MyLinkedList<T> {
 	private Item first;
 
 	private Item end;
-	
-	
-	public int getSize() {
-		return size;
-	}
-	public void setSize(int size) {
-		this.size = size;
-	}
-	public Item getFirst() {
-		return first;
-	}
-	public Item getEnd() {
-		return end;
-	}
-	public void setFirst(Item first) {
+
+
+	private void setFirst(Item first) {
 		this.first = first;
 	}
-	public void setEnd(Item end) {
+	private void setEnd(Item end) {
 		this.end = end;
 	}
 
+	public T get(int index) {
+		return getItem(index).getObj();
+	}
+
+	public int size() {
+
+		int size = 0;
+
+		if(first == null) {
+			return 0;
+		}
+
+		for(Item iter = first; iter != null; iter = iter.getNext(), size++);
+
+		return size;
+
+	}
 
 	public void add(T data) {
 
@@ -69,21 +75,21 @@ public class MyLinkedList<T> {
 			end.setNext(addedItem);
 			end=addedItem;
 		}
-		size++;
+		
 
 	}
-	public Item getItem( int index){
-		if(index < 0 || index >= size) {
+	private Item getItem( int index){
+		if(index < 0 || index >= size()) {
 			throw new IndexOutOfBoundsException("Невозможно получить элемент по индексу " + index);
 		}
-        int i=0;
-        Item item= new Item();
-        item=first;
-        while(i<index){
-        	item=item.getNext();
-        	i++;
-        }
-        return item;
+		int i=0;
+		Item item= new Item();
+		item=first;
+		while(i<index){
+			item=item.getNext();
+			i++;
+		}
+		return item;
 	}
 
 	public void paste (T data, int index){
@@ -92,7 +98,7 @@ public class MyLinkedList<T> {
 		Item item= getItem(index);
 		addedItem.setNext(item);
 		getItem(index-1).setNext(addedItem);
-		size++;
+		
 	}
 	private void delete(int index) {
 		if(first == null) {			
@@ -108,27 +114,27 @@ public class MyLinkedList<T> {
 		else{
 			getItem(index-1).setNext(getItem(index+1));
 		}
-		size--;
+		
 	}
-	
+
 	public static void main(String[] args) {
 		MyLinkedList<Student> myLinkedList = new MyLinkedList<Student>();
 		myLinkedList.add(new Student(10, 1940, "John"));
 		myLinkedList.add(new Student(11, 1941, "bill"));
 		myLinkedList.add(new Student(12, 1940, "Kir"));
-		for (int i=0; i<myLinkedList.getSize(); i++){
-			System.out.println(myLinkedList.getItem(i).getObj().getName());
+		for (int i=0; i<myLinkedList.size(); i++){
+			System.out.println(myLinkedList.get(i).getName());
 		}
-		Student st= (Student)myLinkedList.getItem(2).getObj();
+		Student st= myLinkedList.get(2);
 		myLinkedList.paste(new Student(12, 1940, "Bob"), 1);
 		System.out.println("Paste Bob on the 2nd spot");
-		for (int i=0; i<myLinkedList.getSize(); i++){
-			System.out.println(myLinkedList.getItem(i).getObj().getName());
+		for (int i=0; i<myLinkedList.size(); i++){
+			System.out.println(myLinkedList.get(i).getName());
 		}
 		myLinkedList.delete(2);
 		System.out.println("Delete the 3rd");
-		for (int i=0; i<myLinkedList.getSize(); i++){
-			System.out.println(myLinkedList.getItem(i).getObj().getName());
+		for (int i=0; i<myLinkedList.size(); i++){
+			System.out.println(myLinkedList.get(i).getName());
 		}
 	}
 
