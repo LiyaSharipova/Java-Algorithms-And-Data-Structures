dd3833c3bacae307b74691b03f4cd801ce975279
LiyaSharipova <liyasharipova2107@gmail.com>
2015-04-08 20:59:48 +0300

    .

--- FileManager/src/ru/kpfu/itis/group403/yuskevich/fileManager/classes/Helper.java
+++ FileManager/src/ru/kpfu/itis/group403/yuskevich/fileManager/classes/Helper.java
@@ -7,35 +7,35 @@ import java.io.File;
  */
 public class Helper {
 
-    //должен проверять имя файла на знаки \/|*?:<>"
-    public static boolean checkName(String a){
-        for(int i = 0; i < a.length();i++){
-            if(a.charAt(i)=='/'||a.charAt(i)=='|'||a.charAt(i)=='*'||a.charAt(i)=='?'||
-                    a.charAt(i)=='"'||a.charAt(i)=='<'||a.charAt(i)=='>'||a.charAt(i)=='\\'||a.charAt(i)==':')
-                return false;
-        }
-        return true;
-    }
+	//должен проверять имя файла на знаки \/|*?:<>"
+	public static boolean checkName(String a){
+		for(int i = 0; i < a.length();i++){
+			if(a.charAt(i)=='/'||a.charAt(i)=='|'||a.charAt(i)=='*'||a.charAt(i)=='?'||
+					a.charAt(i)=='"'||a.charAt(i)=='<'||a.charAt(i)=='>'||a.charAt(i)=='\\'||a.charAt(i)==':')
+				return false;
+		}
+		return true;
+	}
 
 
-    //пользователь может ввести либо путь либо конкретное имя файла в текущей директории, этот метод распознает эти 2 случая
-   public static File correctPath(String path,File dir ){
-        boolean directory = false;
-        if(path.substring(0,3).equals("C:\\"))
-            return new File(path);
-        else
-            return new File(dir.getPath()+"/"+path);
-    }
-    public static boolean checkLength(int length, String[] commandWords) throws IllegalArgumentException {
-    	if(commandWords.length==length){
-    		return true;
-    	}
-    	String options="";
-    	for (int i = length; i < commandWords.length; i++) {
+	//пользователь может ввести либо путь либо конкретное имя файла в текущей директории, этот метод распознает эти 2 случая
+	public static File correctPath(String path,File dir ){
+		boolean directory = false;
+		if(path.substring(0,3).equals("C:"+File.separator))
+			return new File(path);
+		else
+			return new File(dir.getPath()+File.separator+path);
+	}
+	public static boolean checkLength(int length, String[] commandWords) throws IllegalArgumentException {
+		if(commandWords.length==length){
+			return true;
+		}
+		String options="";
+		for (int i = length; i < commandWords.length; i++) {
 			options+=commandWords[i]+" ";
 		}
-            throw new IllegalArgumentException("Extra option(s): "+ options) ;// выводит только лишние слова
-    	
+		throw new IllegalArgumentException("Extra option(s): "+ options) ;// выводит только лишние слова
+
 	}
 
 }
